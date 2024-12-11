c908732dd1134d788f104709a84030952cb13495
Haider Mukhtar <haidermukhtarch405@gmail.com>
2024-12-11 14:59:38 +0500

    11/12/2024 - React+Vite+Tailwind

---
+++ react-vite-tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h1>
+        React + Vite + Tailwind CSS
+      </h1>
+    </>
+  )
+}
+
+export default App
